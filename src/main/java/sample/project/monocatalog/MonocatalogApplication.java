7e79c9b20fd63c1f45bee94487c555f75f9dfead
kitou88C39 <kanfutrooper@icloud.com>
2023-04-21 15:44:19 +0900

    first commit

---
+++ src/main/java/sample/project/monocatalog/MonocatalogApplication.java
@@ -0,0 +1,13 @@
+package sample.project.monocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonocatalogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sample/project/monocatalog/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sample.project.monocatalog;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MonocatalogApplication.class);
+	}
+
+}
